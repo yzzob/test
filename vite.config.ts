71690d7fc6bc407c46cd63fe508966b2bccb9151
yzzob <2672510873@qq.com>
2025-04-27 19:16:50 +0800

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/test/', // 替换为你的仓库名
+})
